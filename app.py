b389dde132db6933e594f844be4aa65c4fbf1a60
Ayush Ukhalkar <97335281+iushdoescode@users.noreply.github.com>
2023-09-20 12:02:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -226,7 +226,7 @@ def print_data(pokelist):
     sprites_path = 'https://github.com/iushdoescode/WhosThatPokemon-Deployed/blob/master/Sprites/'
     sprites = []
     for poke in pokelist:
-    response = requests.get(url+poke.lower())
+        response = requests.get(url+poke.lower())
     st.title("This is most likely your Pokemon")
     jresponse = response.json()
     type = jresponse['types'][0]['type']['name']
